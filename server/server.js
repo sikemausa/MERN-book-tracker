9e0a14bce87cfa7107f00a016f2c27d13e69c85c
Mike Sausa <mikejsausa@gmail.com>
2017-04-06 21:18:29 -0600

    added books store and book path

--- server/server.js
+++ server/server.js
@@ -4,9 +4,17 @@ const path = require('path');
 const app = express();
 const morgan = require('morgan');
 
+app.use(morgan('dev'))
 app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+let books = [];
+let id = 0;
+
+app.get('/books', function(req, res){
+  res.json(books);
+});
 
 app.listen(3000);
 console.log('listening on port 3000!!');
